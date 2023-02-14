156cfa6353e4f5bda18f718bd97e01814d8c5b74
francescogentile25 <francescogentile250893@gmail.com>
2023-02-14 15:37:34 +0100

    FEAT dinamic card element

--- js/team.js
+++ js/team.js
@@ -43,7 +43,7 @@ console.log(imageElement)
 const nameElement = document.getElementById('name')
 const jobElement = document.getElementById('job')
 
-
+const cardEl = document.querySelector('.row')
 for (let i = 0; i < team.length; i++) {
 	const currentWorker = team[i]
 	const name =currentWorker.name
@@ -51,5 +51,16 @@ for (let i = 0; i < team.length; i++) {
 	const job =currentWorker.job
 	const image =currentWorker.image
 	console.log(name, job, image)
-    nameElement.innerHtml = name
+    const card = `
+    <div class="col-4 ">
+    <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <p class="card-text"> ${name}</p>
+            <p class="card-text"> ${job}</p>
+        </div>
+    </div>
+</div>
+    `
+    cardEl.innerHTML += card
 }
